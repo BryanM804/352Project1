809f07142b591874c03de9b80cb782584780506e
Ishaan <81937191+5h3r10k@users.noreply.github.com>
2025-02-16 00:34:54 -0500

    updated client code

--- client.py
+++ client.py
@@ -1,6 +1,13 @@
 import socket
 
+input_data_file = "in-proj.txt"
+output_data_file = "out-proj.txt"
+output_data = []
+buffer_size = 200
+
 def client():
+
+    # Attempt to create a client socket
     try:
         cs = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         print("[C]: Client socket created")
@@ -8,20 +15,34 @@ def client():
         print('socket open error: {} \n'.format(err))
         exit()
         
-    # Define the port on which you want to connect to the server
+    # Connect to server
     port = 50007
     localhost_addr = socket.gethostbyname(socket.gethostname())
-
-    # connect to the server on local machine
     server_binding = (localhost_addr, port)
     cs.connect(server_binding)
 
-    # Receive data from the server
-    data_from_server=cs.recv(100)
-    print("[C]: Data received from server: {}".format(data_from_server.decode('utf-8')))
+    # Send data to server
+    # Send line by line, waiting for a response after each line
+    with open(input_data_file, "r") as f:
+        for line in f:
+            line = line.rstrip()
+            print("[C]: Sending data to server: {}".format(line))
+            cs.send(line.encode('utf-8'))
+            data_from_server = cs.recv(buffer_size)
+            print("[C]: Data received from server: {}".format(data_from_server.decode('utf-8')))
+            output_data.append(data_from_server.decode('utf-8'))
 
     # close the client socket
     cs.close()
+    print("[C]: Connection closed")
+
+    # Write output data to file
+    with open(output_data_file, "w") as f:
+        for line in output_data:
+            f.write(line)
+            f.write("\n")
+
     exit()
 
-client()
+if __name__ == "__main__":
+    client()
